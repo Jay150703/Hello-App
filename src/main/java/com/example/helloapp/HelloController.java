3dd054685be534c9acbbee2b5981fac3ce9c598c
Jay150703 <jayanthijemi15@gmail.com>
2026-03-27 14:16:42 +0530

    [Jayanthi] : Completed usecases 1 to 5

---
+++ src/main/java/com/example/helloapp/HelloController.java
@@ -0,0 +1,35 @@
+package com.example.helloapp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // UC1
+    @GetMapping("")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+    // UC2
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    // UC3
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    // UC4
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+    // UC5
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName,
+                              @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
+}
